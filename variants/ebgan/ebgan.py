52df048cac83a88b186b69293a4a08d3055d2c06
Robin <robinjacksonkenyan@gmail.com>
2021-01-05 19:55:51 +0300

    configure data loader

--- variants/ebgan/ebgan.py
+++ variants/ebgan/ebgan.py
@@ -101,4 +101,17 @@ generator.apply(weights_init_normal)
 discriminator.apply(weights_init_normal)
 
 
-
+# Configure data loader
+os.makedirs("../../data/mnist", exist_ok=True)
+dataloader = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../../data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        ),
+    ),
+    batch_size=opt.batch_size,
+    shuffle=True,
+)
