dcf8682b3d688d75b78cfc138aa10eab0d89162a
Robin <robinjacksonkenyan@gmail.com>
2021-01-11 01:02:50 +0300

    training

--- variants/pixelda/pixelda.py
+++ variants/pixelda/pixelda.py
@@ -142,3 +142,167 @@ class Classifier(nn.Module):
         return label
 
 
+# Loss function
+adversarial_loss = torch.nn.MSELoss()
+task_loss = torch.nn.CrossEntropyLoss()
+
+# Loss weights
+lambda_adv = 1
+lambda_task = 0.1
+
+# Initialize generator and discriminator
+generator = Generator()
+discriminator = Discriminator()
+classifier = Classifier()
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    classifier.cuda()
+    adversarial_loss.cuda()
+    task_loss.cuda()
+
+# Initialize weights
+generator.apply(weights_init_normal)
+discriminator.apply(weights_init_normal)
+classifier.apply(weights_init_normal)
+
+# Configure data loader
+os.makedirs("../../data/mnist", exist_ok=True)
+dataloader_A = torch.utils.data.DataLoader(
+    datasets.MNIST(
+        "../../data/mnist",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+        ),
+    ),
+    batch_size=opt.batch_size,
+    shuffle=True,
+)
+
+os.makedirs("../../data/mnistm", exist_ok=True)
+dataloader_B = torch.utils.data.DataLoader(
+    MNISTM(
+        "../../data/mnistm",
+        train=True,
+        download=True,
+        transform=transforms.Compose(
+            [
+                transforms.Resize(opt.img_size),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ]
+        ),
+    ),
+    batch_size=opt.batch_size,
+    shuffle=True,
+)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(
+    itertools.chain(generator.parameters(), classifier.parameters()), lr=opt.lr, betas=(opt.b1, opt.b2)
+)
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
+
+# ----------
+#  Training
+# ----------
+
+# Keeps 100 accuracy measurements
+task_performance = []
+target_performance = []
+
+for epoch in range(opt.n_epochs):
+    for i, ((imgs_A, labels_A), (imgs_B, labels_B)) in enumerate(zip(dataloader_A, dataloader_B)):
+
+        batch_size = imgs_A.size(0)
+
+        # Adversarial ground truths
+        valid = Variable(FloatTensor(batch_size, *patch).fill_(1.0), requires_grad=False)
+        fake = Variable(FloatTensor(batch_size, *patch).fill_(0.0), requires_grad=False)
+
+        # Configure input
+        imgs_A = Variable(imgs_A.type(FloatTensor).expand(batch_size, 3, opt.img_size, opt.img_size))
+        labels_A = Variable(labels_A.type(LongTensor))
+        imgs_B = Variable(imgs_B.type(FloatTensor))
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # Sample noise
+        z = Variable(FloatTensor(np.random.uniform(-1, 1, (batch_size, opt.latent_dim))))
+
+        # Generate a batch of images
+        fake_B = generator(imgs_A, z)
+
+        # Perform task on translated source image
+        label_pred = classifier(fake_B)
+
+        # Calculate the task loss
+        task_loss_ = (task_loss(label_pred, labels_A) + task_loss(classifier(imgs_A), labels_A)) / 2
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = lambda_adv * adversarial_loss(discriminator(fake_B), valid) + lambda_task * task_loss_
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = adversarial_loss(discriminator(imgs_B), valid)
+        fake_loss = adversarial_loss(discriminator(fake_B.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        # ---------------------------------------
+        #  Evaluate Performance on target domain
+        # ---------------------------------------
+
+        # Evaluate performance on translated Domain A
+        acc = np.mean(np.argmax(label_pred.data.cpu().numpy(), axis=1) == labels_A.data.cpu().numpy())
+        task_performance.append(acc)
+        if len(task_performance) > 100:
+            task_performance.pop(0)
+
+        # Evaluate performance on Domain B
+        pred_B = classifier(imgs_B)
+        target_acc = np.mean(np.argmax(pred_B.data.cpu().numpy(), axis=1) == labels_B.numpy())
+        target_performance.append(target_acc)
+        if len(target_performance) > 100:
+            target_performance.pop(0)
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f] [CLF acc: %3d%% (%3d%%), target_acc: %3d%% (%3d%%)]"
+            % (
+                epoch,
+                opt.n_epochs,
+                i,
+                len(dataloader_A),
+                d_loss.item(),
+                g_loss.item(),
+                100 * acc,
+                100 * np.mean(task_performance),
+                100 * target_acc,
+                100 * np.mean(target_performance),
+            )
+        )
+
+        batches_done = len(dataloader_A) * epoch + i
+        if batches_done % opt.sample_interval == 0:
+            sample = torch.cat((imgs_A.data[:5], fake_B.data[:5], imgs_B.data[:5]), -2)
+            save_image(sample, "images/%d.png" % batches_done, nrow=int(math.sqrt(batch_size)), normalize=True)
