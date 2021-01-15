406db1e37519a292b9c3b483160da01c483d9c52
Robin <robinjacksonkenyan@gmail.com>
2021-01-15 22:00:36 +0300

    parser arguments

---
+++ variants/srgan/srgan.py
@@ -0,0 +1,55 @@
+"""
+Super-resolution of CelebA using Generative Adversarial Networks.
+The dataset can be downloaded from: https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADIKlz8PR9zr6Y20qbkunrba/Img/img_align_celeba.zip?dl=0
+(if not available there see if options are listed at http://mmlab.ie.cuhk.edu.hk/projects/CelebA.html)
+Instrustion on running the script:
+1. Download the dataset from the provided link
+2. Save the folder 'img_align_celeba' to '../../data/'
+4. Run the sript using command 'python3 srgan.py'
+"""
+
+import argparse
+import os
+import numpy as np
+import math
+import itertools
+import sys
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image, make_grid
+
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+
+from models import *
+from datasets import *
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+os.makedirs("images", exist_ok=True)
+os.makedirs("saved_models", exist_ok=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--dataset_name", type=str, default="img_align_celeba", help="name of the dataset")
+parser.add_argument("--batch_size", type=int, default=4, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from which to start lr decay")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--hr_height", type=int, default=256, help="high res. image height")
+parser.add_argument("--hr_width", type=int, default=256, help="high res. image width")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=100, help="interval between saving image samples")
+parser.add_argument("--checkpoint_interval", type=int, default=-1, help="interval between model checkpoints")
+opt = parser.parse_args()
+print(opt)
+
+cuda = torch.cuda.is_available()
+
+hr_shape = (opt.hr_height, opt.hr_width)
+
