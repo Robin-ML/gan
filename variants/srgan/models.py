cde223afa141f6e6e3b43ce883397286987fb390
Robin <robinjacksonkenyan@gmail.com>
2021-01-15 21:57:52 +0300

    add feature extractor and residual block

---
+++ variants/srgan/models.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torchvision.models import vgg19
+import math
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self):
+        super(FeatureExtractor, self).__init__()
+        vgg19_model = vgg19(pretrained=True)
+        self.feature_extractor = nn.Sequential(*list(vgg19_model.features.children())[:18])
+
+    def forward(self, img):
+        return self.feature_extractor(img)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_features):
+        super(ResidualBlock, self).__init__()
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(in_features, in_features, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(in_features, 0.8),
+            nn.PReLU(),
+            nn.Conv2d(in_features, in_features, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(in_features, 0.8),
+        )
+
+    def forward(self, x):
+        return x + self.conv_block(x)
+
+
